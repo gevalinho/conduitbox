7de4e53f0b6c1bb686aa422a2d16f11f253e834f
gevalinho <gevalinho1@gmail.com>
2025-09-02 16:32:00 +0100

    implementing services

--- src/components/service/Service.tsx
+++ src/components/service/Service.tsx
@@ -42,7 +42,7 @@ const Service = () => {
                       </div>
 
                       <h6 className="cs_work_title">{item.title}</h6>
-                      <p className="cs_work_subtitle">
+                      <p className="cs_work_subtitle m-2">
                         {item.des}
                       </p>
                     </div>
